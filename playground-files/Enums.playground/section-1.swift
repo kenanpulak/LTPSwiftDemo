435822ba259f65248c338bbde5ce9e4d1e7b59e1
kenanpulak <kenan.pulak@therealreal.com>
2014-11-17 16:49:35 -0800

    added enums

---
+++ playground-files/Enums.playground/section-1.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+enum Days
+{
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+    case Saturday
+    case Sunday
+}
+
+var firstDay = Days.Monday
+firstDay = .Wednesday
+
